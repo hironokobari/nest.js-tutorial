b09b47d0bcd2b66973caa21b00c1d59ce97c73bb
hironokobari <kobari0127@outlook.jp>
2024-09-22 01:10:52 +0900

    CRUD operation implementation

---
+++ src/items/items.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { Item } from './items.model';
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+  @Get()
+  findAll(): Item[] {
+    return this.itemsService.findAll();
+  }
+
+  @Get(':id') // /items/id
+  findById(@Param('id') id: string): Item {
+    return this.itemsService.findById(id);
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: 'ON_SALE',
+    };
+    return this.itemsService.create(item);
+  }
+
+  @Put(':id') // /items/id
+  updateStatus(@Param('id') id: string): Item {
+    return this.itemsService.updateStatus(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    this.itemsService.delete(id);
+  }
+}
